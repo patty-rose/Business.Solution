dc725892e679cab711d557ef7c4c7ffd3bc28b07
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-09-10 14:25:27 -0700

    add Get route in ShopsController

--- Business/Controllers/ShopsController.cs
+++ Business/Controllers/ShopsController.cs
@@ -22,6 +22,26 @@ namespace Business.Controllers
       _db = db;
     }
 
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Shop>>> Get(string name, string shopType, string typeCategory)
+    {
+      var query = _db.Shops.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+      if (shopType != null)
+      {
+        query = query.Where(entry => entry.ShopType == shopType);
+      }
+      if (typeCategory != null)
+      {
+        query = query.Where(entry => entry.TypeCategory == typeCategory);
+      }
+      
+      return await query.ToListAsync();
+    }
 
   }
 }
