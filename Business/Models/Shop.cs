dae8c5fddbb3d592d4f66acbbc0c5d77d9da2871
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-09-10 17:55:55 -0700

    add DataAnnotations and error messages to Shop Model

--- Business/Models/Shop.cs
+++ Business/Models/Shop.cs
@@ -4,12 +4,16 @@ namespace Business.Models
 {
   public class Shop
   {
+    [Required(ErrorMessage = "'ShopId' is required.")]
     public int ShopId { get; set; }
 
+    [Required(ErrorMessage = "'Name' is required."), StringLength(50)]
     public string Name { get; set; }
 
+    [Required(ErrorMessage = "'ShopType' is required."), StringLength(50)]
     public string ShopType { get; set; }
 
+    [Required(ErrorMessage = "'TypeCategory' is required."), StringLength(50)]
     public string TypeCategory { get; set; }
   }
 }
