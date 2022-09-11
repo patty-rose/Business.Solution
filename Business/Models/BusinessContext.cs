63eccb877d6e2f045241f4ddfaa23c6005ef6d82
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-09-10 18:45:34 -0700

    add more seed data to demonstrate pagination

--- Business/Models/BusinessContext.cs
+++ Business/Models/BusinessContext.cs
@@ -19,7 +19,13 @@ namespace Business.Models
         new Shop { ShopId = 2, Name = "Doe Donuts", ShopType = "Restaurant", TypeCategory = "Donuts" },
         new Shop { ShopId = 3, Name = "Mississippi Records", ShopType = "Shop", TypeCategory = "Music" },
         new Shop { ShopId = 4, Name = "Green Zebra", ShopType = "Store", TypeCategory = "Grocery" },
-        new Shop { ShopId = 5, Name = "Babydoll Pizza", ShopType = "Restaurant", TypeCategory = "Pizza" }
+        new Shop { ShopId = 5, Name = "Babydoll Pizza", ShopType = "Restaurant", TypeCategory = "Pizza" },
+        new Shop { ShopId = 6, Name = "New Seasons", ShopType = "Store", TypeCategory = "Grocery" },
+        new Shop { ShopId = 7, Name = "JoJo's", ShopType = "Restaurant", TypeCategory = "Fried Chicken" },
+        new Shop { ShopId = 8, Name = "People's Food Coop", ShopType = "Store", TypeCategory = "Grocery" },
+        new Shop { ShopId = 9, Name = "Academy", ShopType = "Theater", TypeCategory = "Film" },
+        new Shop { ShopId = 10, Name = "Arlene Schintzer", ShopType = "Concert Hall", TypeCategory = "Music" },
+        new Shop { ShopId = 11, Name = "The Laurelhurst Theater", ShopType = "Theater", TypeCategory = "Film" }
       );
     }
   }
